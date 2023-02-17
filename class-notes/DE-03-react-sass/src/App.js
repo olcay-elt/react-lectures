2167041a8ae18ad2a0c7ef8d5cf9baa5864246f1
olcay-elt <olcay.elt@gmail.com>
2023-02-17 12:13:36 +0100

    Add new file

--- class-notes/DE-03-react-sass/src/App.js
+++ class-notes/DE-03-react-sass/src/App.js
@@ -4,6 +4,7 @@
 
 import "./App.scss"
 import Card from "./components/card/Card"
+import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import data from "./data"
 
@@ -12,6 +13,7 @@ function App() {
         <div >
             <Header />
             <Card data={data} />
+            <Footer />
         </div>
     );
 }
---
+++ class-notes/DE-03-react-sass/src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+
+import "./Footer.scss"
+function Footer() {
+    return (
+        <footer>
+            <p>Copyright by Clarusway 2023</p>
+        </footer>
+    )
+}
+
+export default Footer
