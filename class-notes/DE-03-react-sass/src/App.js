cee418513b2120f0d722f3897797fd3f6b75536b
olcay-elt <olcay.elt@gmail.com>
2023-02-17 10:44:06 +0100

    Add

--- class-notes/DE-03-react-sass/src/App.js
+++ class-notes/DE-03-react-sass/src/App.js
@@ -3,12 +3,14 @@
 // import "./scss/_variable.scss"
 
 import "./App.scss"
+import Card from "./components/card/Card"
 import Header from "./components/header/Header";
 
 function App() {
     return (
         <div >
             <Header />
+            <Card />
         </div>
     );
 }
