75fcf049f1acb9b4b39c0c7b246e6dfead572c21
olcay-elt <olcay.elt@gmail.com>
2023-02-17 11:25:05 +0100

    Add

--- class-notes/DE-03-react-sass/src/components/card/Card.jsx
+++ class-notes/DE-03-react-sass/src/components/card/Card.jsx
@@ -1,8 +1,34 @@
 import "./Card.scss"
+//! react jsx alanında (return den sonra) döngü olarak sadece map e izin verilir, condition olarakta ternary ye izin verilir
 
-const Card = () => {
+const Card = ({ data }) => {
+    console.log(data);
+    // bu return react in ekrana bastirma return ü
     return (
-        <div>Card</div>
+        <div className="konteynir">
+            {data.map((veri) => {
+                const { name, job, img, comment } = veri;
+                {/* bu return ise mapte süslü kullandigimiz icin */ }
+                return (
+                    <div className="kart">
+                        <h1>{name}</h1>
+                        <h2>{job}</h2>
+                        <p>{comment}</p>
+                        <img src={img} alt="" />
+
+                        <div className="btn-konteynir">
+                            <button className=" btn-konteynir-small">Small</button>
+                            <button className=" btn-konteynir--large">Large</button>
+                        </div>
+                    </div>
+                )
+
+
+            })}
+
+
+
+        </div>
     )
 }
 
