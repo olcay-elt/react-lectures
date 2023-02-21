a44893d3ad3fdad906f010e88b7188a15b8e7333
olcay-elt <olcay.elt@gmail.com>
2023-02-21 23:19:49 +0100

    Add

--- class-notes/DE-06-events-hooks/src/2-hooks/Hooks.jsx
+++ class-notes/DE-06-events-hooks/src/2-hooks/Hooks.jsx
@@ -1,21 +1,53 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const Hooks = () => {
-    const [sayac, setsayac] = useState(0)
-    const arttir = () => {
-        setsayac(sayac + 1
-        )
-    }
-    return (
-        <div>
-            <h2>*********************************************************</h2>
-            <h1>USESTATE</h1>
-            <h2>COUNT: <Span>{sayac}</Span></h2>
-            <button className='btn btn-primary'>Arttir</button>
-            <button className='btn btn-warning' >Azalt</button>
-            <button className='btn btn-danger'>Temizle</button>
-        </div>
-    )
-}
+    const [sayac, setSayac] = useState(0);
+    const [people, setPeople] = useState({
+        isim: "Sefa",
+        email: "sefa@hotmail.com",
+        yas: 34,
+    });
 
-export default Hooks
+    const arttir = () => {
+        setSayac(sayac + 1);
+    };
+
+    const degistir = () => {
+        setPeople({ isim: "Olcay", email: "olcay@hotmail.com", yas: 25 });
+    };
+    return (
+        <div className="container">
+            <h2>*****************************************************</h2>
+
+            <h1>USESTATE</h1>
+            <h2>
+                {" "}
+                COUNT: <span>{sayac}</span>
+            </h2>
+
+            <button onClick={arttir} className="btn btn-primary">
+                ARTTIR
+            </button>
+
+            <button onClick={() => setSayac(sayac - 1)} className="btn btn-warning">
+                AZALT
+            </button>
+            <h1>#######################################################</h1>
+            <div className="text-center">
+                <h1 className="text-danger">OBJECT İLE USESTATE KULLANIMI</h1>
+
+                <h2>{people.isim}</h2>
+
+                <h5>{people.email}</h5>
+
+                <h5>{people.yas}</h5>
+
+                <button onClick={degistir} className="btn btn-primary">
+                    DEGISTIR
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Hooks;
