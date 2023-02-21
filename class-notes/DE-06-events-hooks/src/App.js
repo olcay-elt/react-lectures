4acf20829b3084834101f96626e445c7489e0143
olcay-elt <olcay.elt@gmail.com>
2023-02-21 23:47:17 +0100

    Add

--- class-notes/DE-06-events-hooks/src/App.js
+++ class-notes/DE-06-events-hooks/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Events from "./1-events-hooks-intro/Events";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Hooks from "./2-hooks/Hooks";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div >
+            <Events />
+
+            <Hooks />
+        </div>
+    );
 }
 
 export default App;
