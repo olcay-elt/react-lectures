3c16aa7a9e21e5cbe3a787749b0e3fd3d71fcce6
olcay-elt <olcay.elt@gmail.com>
2023-02-23 15:46:29 +0100

    Add

--- class-notes/DE-07-nba-legends/src/components/PlayerCard.jsx
+++ class-notes/DE-07-nba-legends/src/components/PlayerCard.jsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import Card from "react-bootstrap/Card";
-
 const PlayerCard = ({ name, img, statistics }) => {
+    const [showImage, setShowImage] = useState(true);
     // const { name, img, statistics } = props
-    const showImage = false;
+    const handleClick = () => setShowImage(!showImage);
     return (
-        <Card className="rounded-2 m-auto player-card">
-            {showImage ? (
+        <Card
+            className="rounded-2 m-auto player-card"
+            onClick={handleClick}
+            //   onClick={() => setShowImage(!showImage)}
+            role="button"
+        >
+            {showImage && (
                 <Card.Img variant="top" src={img} className="player-logo" />
-            ) : (
+            )}
+
+            {!showImage && (
                 <ul className="m-auto">
                     {statistics.map((item, i) => {
                         return (
@@ -18,6 +26,19 @@ const PlayerCard = ({ name, img, statistics }) => {
                     })}
                 </ul>
             )}
+            {/* {showImage ? (
+        <Card.Img variant="top" src={img} className="player-logo" />
+      ) : (
+        <ul className="m-auto">
+          {statistics.map((item, i) => {
+            return (
+              <li className="h5 text-start list-unstyled" key={i}>
+                🏀 {item}
+              </li>
+            );
+          })}
+        </ul>
+      )} */}
             <Card.Footer>
                 <Card.Title>{name}</Card.Title>
             </Card.Footer>
