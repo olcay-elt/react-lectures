94893d34d50b9ff49d18c041fdf3b1942bfa5b8a
olcay-elt <olcay.elt@gmail.com>
2023-02-23 09:26:31 +0100

    Add

--- class-notes/DE-07-nba-legends/src/App.js
+++ class-notes/DE-07-nba-legends/src/App.js
@@ -1,12 +1,10 @@
-// import Footer from "./footer/Footer";
+
 import Msg from "./msg/Msg";
 
 function App() {
     return (
         <div >
-            <Msg />
 
-            <Msg />
         </div>
     );
 }
