af45eb4e6274bb3be09e04949a49b37cf5a94a6d
olcay-elt <olcay.elt@gmail.com>
2023-02-24 10:52:21 +0100

    Add

--- class-notes/DE-08-lifeCycle/src/components/Clock.jsx
+++ class-notes/DE-08-lifeCycle/src/components/Clock.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useState } from 'react';
+import moment from "moment"
 
 const Clock = () => {
+    const [zaman, setZaman] = useState(moment())
+
     return (
-        <div>Clock</div>
+        <div>
+            {zaman.format("HH")}
+            {zaman.format("mm")}
+        </div>
     )
-}
+};
 
 export default Clock;
