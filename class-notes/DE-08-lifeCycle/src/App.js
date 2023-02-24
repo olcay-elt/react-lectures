0f9e31b9f7b746f2cbeb01163af0a23a139f9d60
olcay-elt <olcay.elt@gmail.com>
2023-02-24 11:26:34 +0100

    Add

--- class-notes/DE-08-lifeCycle/src/App.js
+++ class-notes/DE-08-lifeCycle/src/App.js
@@ -2,10 +2,11 @@ import React from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
 import LifeCycleMethods from './components/LifeCyclesMethods';
 import Clock from "./components/Clock"
+import "./App.css"
 function App() {
     return (
-        <div>
-            <LifeCycleMethods />
+        <div className='container text-center mt-4'>
+            <button></button>            {/* <LifeCycleMethods /> */}
             <Clock />
         </div>
     );
--- class-notes/DE-08-lifeCycle/src/components/Clock.jsx
+++ class-notes/DE-08-lifeCycle/src/components/Clock.jsx
@@ -1,16 +1,58 @@
-import React from 'react'
-import { useState } from 'react';
+// //?==================================================================
+// //?                       USEEFFECT HOOK
+// //?==================================================================
+// //! The Effect Hook,function componentlerde yan etkiler gerçekleştirmenizi sağlar.
+// //! useEffect Hook'u componentDidMount, componentDidUpdate ve componentWillUnmount olarak düşünebiliriz..setInterval, fetch axios ile veri çekme ve mesela dizi her değiştiğinde set et (istediğim bişeyi yap) demek için kullanılır
+
+// //! useEffect(() => {
+// //*   /* ComponentDidMount code */
+// //! }, []);
+
+// //! useEffect(() => {
+// //*   */ componentDidUpdate code */
+// //! }, [var1, var2]);
+
+// //! useEffect(() => {
+// //!   return () => {
+// //*     //* componentWillUnmount code */
+// //!   };
+// //! }, []);
+
+// //! useEffect(() => {
+// //*   //* componentDidMount code + componentDidUpdate code */
+
+// //!   return () => {
+// //*     //* componentWillUnmount code */
+// //!   };
+// //! }, [var1, var2]);
+
+import React, { useEffect, useState } from "react";
 import moment from "moment"
-
 const Clock = () => {
-    const [zaman, setZaman] = useState(moment())
 
-    return (
-        <div>
-            {zaman.format("HH")}
-            {zaman.format("mm")}
-        </div>
-    )
+    const [zaman, setZaman] = useState(moment())
+    const [count, setCount] =
+        //!comppnentDidMount=ilk render da çalış birdaha burayı görme
+        useEffect(() => {
+            setInterval(() => {
+                setZaman(moment());
+                console.log("merhaba");
+            }, 1000);
+
+
+        }, [])
+
+
+
+
+    return <div>
+        {zaman.format("HH")}:
+        {zaman.format("mm")}:
+        {zaman.format("ss")}
+
+        <div><h1>{count}</h1></div>
+
+    </div>;
 };
 
 export default Clock;
