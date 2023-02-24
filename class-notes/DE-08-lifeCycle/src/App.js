313a9a52bcdeffdaeab3b403e2bebc69e94db2e4
olcay-elt <olcay.elt@gmail.com>
2023-02-24 16:09:00 +0100

    Add

--- class-notes/DE-08-lifeCycle/src/App.js
+++ class-notes/DE-08-lifeCycle/src/App.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import LifeCycleMethods from "./components/LifeCycleMethods";
 import "bootstrap/dist/css/bootstrap.min.css";
-import LifeCycleMethods from './components/LifeCyclesMethods';
-import Clock from "./components/Clock"
-import "./App.css"
+import Clock from "./components/Clock";
+import "./App.css";
+import { useState } from "react";
+import People from "./components/People";
 function App() {
+    const [goster, setGoster] = useState(true);
+
     return (
-        <div className='container text-center mt-4'>
-            <button></button>            {/* <LifeCycleMethods /> */}
-            <Clock />
+        <div>
+            <div className="container text-center mt-4">
+                {/* <button className="btn btn-danger" onClick={() => setGoster(!goster)}>
+        {goster ? "SAKLA" : "GOSTER"}
+      </button> */}
+                {/* <LifeCycleMethods/> */}
+
+                {/* {goster ?    <Clock/> : ""} */}
+                {/* 2.gösterim yolu */}
+                {/* {goster && <Clock />} */}
+
+                {/* && iki parametrede doğruyken kullanılır, || soldaki doğruysa yada
+      sağdaki doğruysa, hangisi true ise onu al */}
+            </div>
+            <People />
         </div>
     );
 }
 
-export default App
+export default App;
