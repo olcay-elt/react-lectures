dd61a2daad859386a7580d26b970167e7071d3d2
olcay-elt <olcay.elt@gmail.com>
2023-02-24 16:06:16 +0100

    Add

--- class-notes/DE-08-lifeCycle/src/components/Clock.jsx
+++ class-notes/DE-08-lifeCycle/src/components/Clock.jsx
@@ -27,32 +27,51 @@
 // //! }, [var1, var2]);
 
 import React, { useEffect, useState } from "react";
-import moment from "moment"
+import moment from "moment";
 const Clock = () => {
+    const [zaman, setZaman] = useState(moment());
+    const [count, setCount] = useState(0);
+    //!comppnentDidMount=ilk render da çalış birdaha burayı görme
+    useEffect(() => {
+        const time = setInterval(() => {
+            setZaman(moment());
+            console.log("merhaba");
+        }, 1000);
 
-    const [zaman, setZaman] = useState(moment())
-    const [count, setCount] =
-        //!comppnentDidMount=ilk render da çalış birdaha burayı görme
-        useEffect(() => {
-            setInterval(() => {
-                setZaman(moment());
-                console.log("merhaba");
-            }, 1000);
+        //!component kapandığında yada başka sayfaya gidildiğinde çalışma dursun istersek
+        return () => {
+            clearInterval(time);
+            console.log("başka sayfaya gidildiği için interval öldü");
+        };
+    }, []);
+    //!count değişkeni her güncellendiğinde alttakiler olsun
+    useEffect(() => {
+        console.log("count değişti");
 
 
-        }, [])
+    }, [count])
 
 
+    return (
+        <div>
+            {zaman.format("HH")}
 
+            {zaman.format("ss") % 2 === 0 ? ":" : " "}
 
-    return <div>
-        {zaman.format("HH")}:
-        {zaman.format("mm")}:
-        {zaman.format("ss")}
+            {zaman.format("mm")}
 
-        <div><h1>{count}</h1></div>
+            {zaman.format("ss") % 2 === 0 ? ":" : " "}
 
-    </div>;
+            {zaman.format("ss")}
+
+            <div className="bg-secondary">
+                <button className="btn btn-warning" onClick={() => setCount(count + 1)}>
+                    ARTTIR
+                </button>
+                <h1>{count}</h1>
+            </div>
+        </div>
+    );
 };
 
 export default Clock;
