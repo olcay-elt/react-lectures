038671c68599b550718a16bb6316eeb1643df855
olcay-elt <olcay.elt@gmail.com>
2023-02-24 10:07:32 +0100

    Add

--- class-notes/DE-08-lifeCycle/src/components/LifeCyclesMethods.jsx
+++ class-notes/DE-08-lifeCycle/src/components/LifeCyclesMethods.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
-
 //?==================================================================
 //?                         LIFECYCLE METHODS
 //?          https://reactjs.org/docs/react-component.html
-//?
+//?         
 //?==================================================================
 
 //* Lifecycle yöntemleri, DOM'daki süreleri boyunca componentler üzerinde çalışmak için kullanılan, React'te yerleşik özel yöntemlerdir.
@@ -15,7 +13,10 @@ import React from 'react'
 //* (Optional) Componentin update edilmesi (componentDidUpdate)
 //* componentin ölümü (DOM ağacından kaldırılıyor)
 
-class LifecycleMethods extends React.Component {
+import React from "react";
+
+class LifeCycleMethods extends React.Component {
+    //! 1-) componentin oluşturulmasında çağrılır
     constructor(props) {
         console.log("Constructor is running");
         super(props);
@@ -23,28 +24,44 @@ class LifecycleMethods extends React.Component {
             count: 0,
         };
     }
+    //????????????
+    handleInc = () => {
+        this.setState({
+            count: this.state.count + 1,
+        });
+    };
 
+    //! 3-) component  monte edildiğinde çağrılır
+    //! (ilk renderdan hemen sonra).
+    //!  lifecycle döngüsünde yalnızca bir kez çağrılır
+    componentDidMount() {
+        console.log("Component kuruldu");
+    }
+
+    //! 4-) bileşen güncellenir ve yeniden oluşturulur hemen sonrasında çağrılır.
+    //! It receives the prevState and prevProps as parameters
+    //!ayrıca yeni değiştirilen duruma veya props lara  bir kez erişebilir
+    componentDidUpdate() {
+        console.log("Component update edildi");
+    }
+
+    //! 5-) bileşen DOM'dan kaldırılmadan önce çağrılır
+    componentWillUnmount() {
+        console.log("Component öldü");
+    }
+
+    //! 2-) her oluşturmada çağrılır (Bileşeni DOM'a çizme)
+    render() {
+        console.log("Component Rendered");
+        return (
+            <div className="container text-center mt-4">
+                <h1 className="text-danger">LIFECYCLE METHODS</h1>
+                <h3>COUNT={this.state.count}</h3>
+                <button className="btn btn-info" onClick={this.handleInc}>
+                    INC
+                </button>
+            </div>
+        );
+    }
 }
-
-
-//! 2-) her oluşturmada çağrılır (Bileşeni DOM'a çizme)
-render() {
-    console.log("Component Rendered");
-    return (
-        <div className="container text-center mt-4">
-            <h1 className="text-danger">LIFECYCLE METHODS</h1>
-            <h3>COUNT={this.state.count}</h3>
-            <button className="btn btn-info" onClick={this.handleInc}>
-                INC
-            </button>
-        </div>
-    );
-}
-
-//! 3-) component  monte edildiğinde çağrılır
-//! (ilk renderdan hemen sonra).
-//!  lifecycle döngüsünde yalnızca bir kez çağrılır
-componentDidMount() {
-    console.log("Component kuruldu");
-}
-export default LifeCyclesMethods
+export default LifeCycleMethods;
