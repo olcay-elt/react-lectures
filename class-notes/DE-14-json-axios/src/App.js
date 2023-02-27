5f044a54e8e6cd9ae25a9e708b519c2451760036
olcay-elt <olcay.elt@gmail.com>
2023-02-27 08:54:23 +0100

    Add new file

---
+++ class-notes/DE-14-json-axios/src/App.js
@@ -0,0 +1,14 @@
+// import Footer from "./footer/Footer";
+import Msg from "./msg/Msg";
+
+function App() {
+    return (
+        <div >
+            <Msg />
+
+            <Msg />
+        </div>
+    );
+}
+
+export default App;
