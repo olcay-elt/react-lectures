e45a49f152b1967fd83835babc73e2bb705a7825
olcay-elt <olcay.elt@gmail.com>
2023-03-02 11:02:53 +0100

    Add new file

--- class-notes/DE-12-13-react-router/src/components/MyNavbar.jsx
+++ class-notes/DE-12-13-react-router/src/components/MyNavbar.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import { Navbar, Container, Nav } from 'react-bootstrap'
+import logo from "../img/logo.png"
 const MyNavbar = () => {
-  return (
-    <div>MyNavbar</div>
-  )
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container fluid>
+                <Navbar.Brand href="#">
+                    <img src={logo} alt="" />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="ms-auto my-2 my-lg-0"
+                        style={{ maxHeight: "100px" }}
+                        navbarScroll
+                    >
+                        <Nav.Link href="#action1">Home</Nav.Link>
+                        <Nav.Link href="#action2">People</Nav.Link>
+
+                    </Nav>
+
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
 
 export default MyNavbar
--- class-notes/DE-12-13-react-router/src/components/Slider.jsx
+++ class-notes/DE-12-13-react-router/src/components/Slider.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { Carousel } from 'react-bootstrap'
+
+import isa from '../img/isa.jpeg'
+import devops from '../img/devops.jpeg'
+import about from '../img/about.jpeg'
+
 
 const Slider = () => {
-  return (
-    <div>Slider</div>
-  )
+    return (
+        <Carousel>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={devops}
+                    alt="First slide"
+                />
+
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={about}
+                    alt="Second slide"
+                />
+
+
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={isa}
+                    alt="Third slide"
+                />
+
+
+            </Carousel.Item>
+        </Carousel>
+    );
 }
 
 export default Slider
