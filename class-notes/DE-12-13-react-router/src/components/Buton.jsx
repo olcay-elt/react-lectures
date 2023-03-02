4ae9c023a5e9a5f10436f539ab6a9c68a431883d
olcay-elt <olcay.elt@gmail.com>
2023-03-02 10:46:52 +0100

    Add

---
+++ class-notes/DE-12-13-react-router/src/components/Buton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+
+const Buton = () => {
+  return (
+    <div>
+buton
+    </div>
+  )
+}
+
+export default Buton
---
+++ class-notes/DE-12-13-react-router/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Buton from '../components/Buton'
+import Slider from '../components/Slider'
+
+const Home = () => {
+  return (
+    <div>
+home
+    </div>
+  )
+}
+
+export default Home
