334c725e4d4a68d61a5b35650574ee6434a5d9e0
olcay-elt <olcay.elt@gmail.com>
2023-03-02 19:55:56 +0100

    Add

--- class-notes/DE-12-13-react-router/src/components/Footer.jsx
+++ class-notes/DE-12-13-react-router/src/components/Footer.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { Navbar } from 'react-bootstrap'
 
 const Footer = () => {
-  return (
-    <div>
-<Navbar  className="justify-content-center bg-dark p-4 text-light fixed-bottom">
+    return (
+        <div>
+            <Navbar className="justify-content-center bg-dark p-4 text-light fixed-bottom">
 
- <p > Copyright {new Date().getFullYear()} </p>
-</Navbar>
+                <p > Copyright {new Date().getFullYear()} </p>
+            </Navbar>
 
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Footer
--- class-notes/DE-12-13-react-router/src/components/MyNavbar.jsx
+++ class-notes/DE-12-13-react-router/src/components/MyNavbar.jsx
@@ -1,14 +1,18 @@
-import { Navbar, Container, Nav } from 'react-bootstrap'
-import { NavLink } from 'react-router-dom';
-import logo from "../img/logo.png"
+//!!!React ta 3 türlü link kullanılabilir. NavLink Link ve a etiketi.hangisini ne zaman kullanmalısınız? React uygulamanız içinde, etkinken stil gerektirmeyen bir bağlantı oluşturmak istiyorsanız, Link bileşenini kullanın. React uygulamanız içinde, etkin durumdayken stil gerektiren bir bağlantı oluşturmak istiyorsanız, NavLink bileşenini kullanın. Son olarak, React uygulamanızda harici bir sayfaya veya rota olmayan bir sayfaya bağlantı oluşturmak istiyorsanız, a  etiketini kullanın.
+
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../img/logo.png";
+
+//!react-bootstrap MyNavbar component
 
 const MyNavbar = () => {
     return (
         <Navbar bg="light" expand="lg">
             <Container fluid>
-                <Navbar.Brand href="#">
+                <Link to="/">
                     <img src={logo} alt="" />
-                </Navbar.Brand>
+                </Link>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
                     <Nav
@@ -16,15 +20,42 @@ const MyNavbar = () => {
                         style={{ maxHeight: "100px" }}
                         navbarScroll
                     >
-                        <NavLink to="/" className="nav-link">Home</NavLink>
-                        <NavLink href="#action2">People</NavLink>
+                        <NavLink
+                            to="/"
+                            className="nav-link"
+                            style={({ isActive }) => ({ color: isActive && "red" })}
+                        >
+                            Home
+                        </NavLink>
 
+                        <NavLink
+                            to="/teacher"
+                            className="nav-link"
+                            style={({ isActive }) => ({ backgroundColor: isActive && "red" })}
+                        >
+                            Teacher
+                        </NavLink>
+
+                        <NavLink
+                            to="/courses"
+                            className="nav-link"
+                            style={({ isActive }) => ({ color: isActive && "red" })}
+                        >
+                            Courses
+                        </NavLink>
+
+                        <NavLink
+                            to="/contact"
+                            className="nav-link"
+                            style={({ isActive }) => ({ fontSize: isActive && "50px" })}
+                        >
+                            Contact
+                        </NavLink>
                     </Nav>
-
                 </Navbar.Collapse>
             </Container>
         </Navbar>
     );
-}
+};
 
-export default MyNavbar
+export default MyNavbar;
