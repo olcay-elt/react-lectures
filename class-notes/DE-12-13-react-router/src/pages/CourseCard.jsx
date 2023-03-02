c8b276b18e1a13adf37abb2e872d7bde9c22c636
olcay-elt <olcay.elt@gmail.com>
2023-03-02 11:11:24 +0100

    Add new file

--- class-notes/DE-12-13-react-router/src/pages/CourseCard.jsx
+++ class-notes/DE-12-13-react-router/src/pages/CourseCard.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import { Container, Col, Card, Row, Button } from 'react-bootstrap'
+import data from "../data"
 const CourseCard = () => {
-  return (
-    <div>CourseCard</div>
-  )
+    return (
+        <Container className="text-center mt-4 p-4 ">
+            <Row className="g-3">
+                {data.map(({ img, name, text, id }) => {
+                    return (
+                        <Col sm={12} md={6} lg={4} key={id}>
+                            <Card style={{ width: "18rem" }}>
+                                <Card.Img variant="top" src={img} />
+                                <Card.Body>
+                                    <Card.Title>{name}</Card.Title>
+                                    <Card.Text>{text}</Card.Text>
+                                    <Button variant="primary">DETAYLAR</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    );
+                })}
+            </Row>
+        </Container>
+    );
 }
 
 export default CourseCard
--- class-notes/DE-12-13-react-router/src/pages/Teacher.jsx
+++ class-notes/DE-12-13-react-router/src/pages/Teacher.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const Teacher = () => {
-  return (
-    <div>Teacher</div>
-  )
-}
+    const [insanlar, setInsanlar] = useState([]);
 
-export default Teacher
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((res) => res.json())
+            .then((data) => setInsanlar(data))
+            .catch((err) => console.log(err));
+    }, []);
+
+    console.log(insanlar);
+    return (
+        <div className="container">
+            <div className="row">
+                {insanlar.map(({ id, name, phone }) => {
+                    return (
+                        <div className="col-12 col-sm-6 col-md-4 col-lg-2" key={id}>
+                            <img
+                                src={`https://avatars.dicebear.com/api/avataaars/${id}.svg`}
+                                alt=""
+                            />
+                            <h1>{name}</h1>
+                            <h5>{phone}</h5>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default Teacher;
