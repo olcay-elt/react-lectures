c0160e56b9a7241d4be4eb6a0a63af28221046a3
olcay-elt <olcay.elt@gmail.com>
2023-03-02 10:51:55 +0100

    Add new file

--- class-notes/DE-12-13-react-router/src/App.js
+++ class-notes/DE-12-13-react-router/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import "bootstrap/dist/css/bootstrap.min.css";
 const App = () => {
-  return (
-    <div>App</div>
-  )
+    return (
+        <div>App</div>
+    )
 }
 
 export default App
