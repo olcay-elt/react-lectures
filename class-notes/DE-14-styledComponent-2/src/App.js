8aba5b4138e5ca86dbc634569289ba5cbe8ce932
olcay-elt <olcay.elt@gmail.com>
2023-03-06 08:53:01 +0100

    Add new folder

---
+++ class-notes/DE-14-styledComponent-2/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "./components/Card";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Card />
+    </div>
+  );
+};
+
+export default App;
