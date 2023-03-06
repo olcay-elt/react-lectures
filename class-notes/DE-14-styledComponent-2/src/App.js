81f94efad608716e0dd0e3eda7662843b18c799b
olcay-elt <olcay.elt@gmail.com>
2023-03-06 15:10:27 +0100

    Add

--- class-notes/DE-14-styledComponent-2/src/App.js
+++ class-notes/DE-14-styledComponent-2/src/App.js
@@ -1,14 +1,26 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
 import Card from "./components/Card";
 import Header from "./components/Header";
+import ContainerSSS from "./components/styles/ContainerSSS";
+
+const style = {
+    colors: {
+        logo: "orange",
+        header: "lightblue",
+    },
+    responsive: "724px"
+};
 
 const App = () => {
-  return (
-    <div>
-      <Header />
-      <Card />
-    </div>
-  );
+    return (
+        <ThemeProvider theme={style}>
+            <ContainerSSS>
+                <Header />
+                <Card />
+            </ContainerSSS>
+        </ThemeProvider>
+    );
 };
 
 export default App;
--- class-notes/DE-14-styledComponent-2/src/components/Header.jsx
+++ class-notes/DE-14-styledComponent-2/src/components/Header.jsx
@@ -1,3 +1,4 @@
+import ButonSSS from "./styles/ButonSSS";
 import DisplaySSS from "./styles/DisplaySSS";
 import ImageSSS, { LogoSSS } from "./styles/ImageSSS";
 
@@ -5,27 +6,26 @@ const Header = () => {
     return (
         <div>
             <DisplaySSS>
-                <LogoSSS src="./images/logo.jpg"
-                <img src="" alt="" />
+                <LogoSSS src="./images/logo.png"></LogoSSS>
 
                 <div>
-                    <button>Apply Courses</button>
-                    <button>Talk to Adviser</button>
+                    <ButonSSS colorOlcay="#A62440">Apply Courses</ButonSSS>
+                    <ButonSSS bgAdem="#A62440">Talk to Adviser</ButonSSS>
                 </div>
             </DisplaySSS>
             <hr />
             <DisplaySSS>
                 <div>
-
                     <h1>The IT Career of Your Dreams Starts Here!</h1>
                     <p>
                         Clarusway is a leading international software Bootcamp. Join a micro
                         class online with other trainees and learn coding skills with a
                         highly-skilled instructor.
                     </p>
-                    <button>Start</button>
+                    <ButonSSS bgAdem="#A62440">Start Your New Carrier</ButonSSS>
                 </div>
-                <ImageSSS
+
+                <ImageSSS src="./images/hero.jpg"></ImageSSS>
             </DisplaySSS>
         </div>
     );
