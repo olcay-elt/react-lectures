0b725715e9adf0583872bf1a0d1a30ecc2662ffa
olcay-elt <olcay.elt@gmail.com>
2023-03-06 09:59:21 +0100

    Add new style

---
+++ class-notes/DE-14-styledComponent-1/src/components/styles/ButonStyle.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const ButtonStyle = styled.button`
+background-color: orange;
+  color: blue;
+  cursor: pointer;
+  padding: 1rem 1.5rem;
+  border: 2px solid green;
+  border-radius: 5px;
+  font-family: "tahoma";
+  font-size: 30px;
+
+`;
+
+export default ButtonStyle;
---
+++ class-notes/DE-14-styledComponent-1/src/components/styles/HeaderStyle.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const HeaderStyle = styled.h1`
+color:red;
+font-size: 2.5rem;
+margin: 1rem 0;
+
+`;
+export const Linkstyle = styled.a`
+color= salmon;
+text-decoration: none;
+
+`
+export default H1;
