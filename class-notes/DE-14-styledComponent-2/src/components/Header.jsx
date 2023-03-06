9f67adf3fd6354a1d8bb5d2d6f5bb3efe281dd1e
olcay-elt <olcay.elt@gmail.com>
2023-03-06 11:03:17 +0100

    Add

--- class-notes/DE-14-styledComponent-2/src/components/Header.jsx
+++ class-notes/DE-14-styledComponent-2/src/components/Header.jsx
@@ -1,8 +1,28 @@
+import DisplaySSS from "./styles/DisplaySSS";
 
 const Header = () => {
-  return (
-   <div>Header</div>
-  );
+    return (
+        <div>
+            <DisplaySSS>
+                <img src="" alt="" />
+
+                <div>
+                    <button>Apply Courses</button>
+                    <button>Talk to Adviser</button>
+                </div>
+            </DisplaySSS>
+            <hr />
+            <DisplaySSS>
+                <div>
+                    <h1>The IT Career of Your Dreams</h1>
+                    <p>sdkjfclsdjclksdncklsdjklsdn</p>
+                    <button>Start</button>
+                </div>
+
+                <img src="" alt="" />
+            </DisplaySSS>
+        </div>
+    );
 };
 
 export default Header;
