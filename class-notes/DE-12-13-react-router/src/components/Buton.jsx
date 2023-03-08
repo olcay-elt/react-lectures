aaa9e7347a67e56abce7aacac3b2542242031b44
olcay-elt <olcay.elt@gmail.com>
2023-03-08 20:45:27 +0100

    Add

--- class-notes/DE-12-13-react-router/src/components/Buton.jsx
+++ class-notes/DE-12-13-react-router/src/components/Buton.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
 import { Button } from 'react-bootstrap'
+import React from 'react'
+
 
 const Buton = () => {
-  return (
-    <div>
-buton
-    </div>
-  )
+    return (
+        <div>
+            <Button className="btn btn-danger p-4 m-2">AC</Button>
+            <Button className="btn btn-danger p-4 m-2">KAPA</Button>
+            <Button className="btn btn-danger p-4 m-2">SİL</Button>
+        </div>
+    );
 }
 
 export default Buton
