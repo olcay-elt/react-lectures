d7ee60050c0ead242831ff8b2d751acf39dac8ef
olcay-elt <olcay.elt@gmail.com>
2023-03-08 15:37:39 +0100

    Add

--- class-notes/DE-15-16-RecipeAppProject-starter/src/pages/login/Login.jsx
+++ class-notes/DE-15-16-RecipeAppProject-starter/src/pages/login/Login.jsx
@@ -1,32 +1,45 @@
 import React from "react";
 import {
-  FormContainer,
-  Header,
-  LoginContainer,
-  StyledButton,
-  StyledForm,
-  StyledImg,
-  StyledInput,
+    FormContainer,
+    Header,
+    LoginContainer,
+    StyledButton,
+    StyledForm,
+    StyledImg,
+    StyledInput,
 } from "./LoginStyles";
 import mealSvg from "../../assets/meal.svg";
 
 const Login = () => {
-  return (
-    <LoginContainer>
-      <FormContainer>
-        <StyledImg src={mealSvg} />
-        <Header>{"<Clarusway/>"}Recipe</Header>
+    return (
+        <LoginContainer>
+            <FormContainer>
+                <StyledImg src={mealSvg} />
+                <Header>{"<Clarusway/>"}Recipe</Header>
 
-        <StyledForm>
-          <StyledInput type="text" placeholder="username" required />
+                <StyledForm>
+                    <StyledInput type="text" placeholder="username" required />
 
-          <StyledInput type="password" placeholder="password" required />
+                    <StyledInput type="password" placeholder="password" required />
 
-          <StyledButton type="submit">Login</StyledButton>
-        </StyledForm>
-      </FormContainer>
-    </LoginContainer>
-  );
+                    <StyledButton type="submit">Login</StyledButton>
+                </StyledForm>
+            </FormContainer>
+        </LoginContainer>
+    );
 };
 
 export default Login;
+
+
+function User(props) {
+    return (
+        <h1>
+            {
+                props.isLogged ? ` ${props.name}` : "Giris yapmadiniz"
+            }
+        </h1>
+    )
+}
+
+export default User;
