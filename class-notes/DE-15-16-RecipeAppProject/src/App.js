ca335fb41e854f98ef450eee979ea7372ee4cb2e
olcay-elt <olcay.elt@gmail.com>
2023-03-09 11:04:43 +0100

    Add

--- class-notes/DE-15-16-RecipeAppProject/src/App.js
+++ class-notes/DE-15-16-RecipeAppProject/src/App.js
@@ -1,23 +1,24 @@
 
 import React from 'react'
-import {BrowserRouter,Route,Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Navbar from './components/navbar/Navbar'
 import Login from './pages/login/Login'
 import About from './pages/about/About'
 const App = () => {
-  return (
-    <BrowserRouter>
-<Navbar/>
+    return (
+        <BrowserRouter>
+            <Navbar />
 
-    <Routes>
-<Route path="/" element={<Login/>}/>
-<Route path="/home" element={<Home/>}/>
-<Route path="/about" element={<About/>}/>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route></Route>
 
-    </Routes>
-    </BrowserRouter>
-  )
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
--- class-notes/DE-15-16-RecipeAppProject/src/pages/home/RecipeCard.jsx
+++ class-notes/DE-15-16-RecipeAppProject/src/pages/home/RecipeCard.jsx
@@ -1,19 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { Button, Cards, RecipeHeader, RecipeImage } from "./HomeStyles";
 
 const RecipeCard = ({ i }) => {
-  console.log(i);
-  return (
-    <Cards>
-      <RecipeHeader>{i.label}</RecipeHeader>
-      <RecipeImage src={i.image} />
-      <Button
-       
-      >
-        Details
-      </Button>
-    </Cards>
-  );
+    console.log(i);
+    let navigate = useNavigate
+    return (
+        <Cards>
+            <RecipeHeader>{i.label}</RecipeHeader>
+            <RecipeImage src={i.image} />
+            <Button onClick={() => { navigate("/details", { state: { i } }) }}
+
+            >
+                Details
+            </Button>
+        </Cards>
+    );
 };
 
 export default RecipeCard;
