3bd12629ba7f845cb38f32cf03938c3fc7e6dbfe
olcay-elt <olcay.elt@gmail.com>
2023-03-09 09:23:44 +0100

    Add

--- class-notes/DE-15-16-RecipeAppProject/src/pages/home/Home.jsx
+++ class-notes/DE-15-16-RecipeAppProject/src/pages/home/Home.jsx
@@ -10,36 +10,36 @@ const APP_ID = "bfbb3efc";
 const APP_KEY = "43faeee790f26cd82b28050d3031619d";
 
 const Home = () => {
-  const [query, setQuery] = useState("");
-  const [ögün, setOgun] = useState("Breakfast");
+    const [query, setQuery] = useState("");
+    const [ögün, setOgun] = useState("Breakfast");
 
-  const [yemekler, setYemekler] = useState([]);
-  // query=yazdığımız sorgu kelimesi, mealType=breakfast vs
-  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${ögün}`;
+    const [yemekler, setYemekler] = useState([]);
+    // query=yazdığımız sorgu kelimesi, mealType=breakfast vs
+    const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${ögün}`;
 
-  const getData = async () => {
-    const veri = await axios.get(url);
-    setYemekler(veri.data.hits);
-    //  console.log(veri);
-  };
-  console.log(yemekler);
-  return (
-    <div>
-      <Header setQuery={setQuery} setOgun={setOgun} getData={getData} />
+    const getData = async () => {
+        const veri = await axios.get(url);
+        setYemekler(veri.data.hits);
+        //  console.log(veri);
+    };
+    console.log(yemekler);
+    return (
+        <div>
+            <Header setQuery={setQuery} setOgun={setOgun} getData={getData} />
 
-      {yemekler.length > 0 ? (
-        <MainContainer>
-          {yemekler.map((i, index) => (
-            <RecipeCard key={index} i={i.recipe} />
-          ))}
-        </MainContainer>
-      ) : (
-        <ImgDiv>
-          <HomeImg src={homeSvg} />
-        </ImgDiv>
-      )}
-    </div>
-  );
+            {yemekler.length > 0 ? (
+                <MainContainer>
+                    {yemekler.map((i, index) => (
+                        <RecipeCard key={index} i={i.recipe} />
+                    ))}
+                </MainContainer>
+            ) : (
+                <ImgDiv>
+                    <HomeImg src={homeSvg} />
+                </ImgDiv>
+            )}
+        </div>
+    );
 };
 
 export default Home;
