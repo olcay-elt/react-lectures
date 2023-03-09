10eeab1a53bc6ac56dfa3f226cd557b119514fd8
olcay-elt <olcay.elt@gmail.com>
2023-03-09 21:23:15 +0100

    Add

---
+++ class-notes/DE-15-16-RecipeAppProject/src/router/PrivateRouter.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PrivateRouter = () => {
+    return (
+        (localStorage.getItem("userName") === "ipek" && localStorage.getItem()
+        )
+}
+
+export default PrivateRouter
