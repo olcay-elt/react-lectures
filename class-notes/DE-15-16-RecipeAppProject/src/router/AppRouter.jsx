d3cb6811377496a481928a115d7ad4ea5873890a
olcay-elt <olcay.elt@gmail.com>
2023-03-09 21:37:38 +0100

    Add

--- class-notes/DE-15-16-RecipeAppProject/src/router/AppRouter.jsx
+++ class-notes/DE-15-16-RecipeAppProject/src/router/AppRouter.jsx
@@ -5,6 +5,8 @@ import Home from "../pages/home/Home";
 import About from "../pages/about/About";
 import Details from "../pages/details/Details";
 import Login from "../pages/login/Login";
+import PrivateRouter from "./PrivateRouter";
+import Footer from "../components/footer/Footer";
 const AppRouter = () => {
     return (
         <BrowserRouter>
@@ -13,11 +15,17 @@ const AppRouter = () => {
             <Routes>
                 <Route path="/" element={<Login />} />
 
-                <Route path="/home" element={<Home />} />
-                <Route path="/about" element={<About />} />
+                <Route path="/home" element={<PrivateRouter />}>
+                    <Route path="" element={<Home />} />
+                </Route>
+
+                <Route path="/about" element={<PrivateRouter />}>
+                    <Route path="" element={<About />} />
+                </Route>
 
                 <Route path="/details" element={<Details />} />
             </Routes>
+            <Footer />
         </BrowserRouter>
     );
 };
--- class-notes/DE-15-16-RecipeAppProject/src/router/PrivateRouter.jsx
+++ class-notes/DE-15-16-RecipeAppProject/src/router/PrivateRouter.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
 
 const PrivateRouter = () => {
     return (
-        (localStorage.getItem("userName") === "ipek" && localStorage.getItem()
-        )
+        (localStorage.getItem("userName") === "ipek" && localStorage.getItem("password") === "1234") ? (<Outlet />) : (<Navigate to="/" />)
+    )
 }
 
+
+// kullanici doğru şifre ve isim girdiyse Outlet komponenti sayesinde istenilen sayfaya git, yanlışlık varsa Navigate ile bulunduğun yerde (Login) kal ya da başka path veriledebilir
 export default PrivateRouter
