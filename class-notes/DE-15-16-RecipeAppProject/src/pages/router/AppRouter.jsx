cc6dff767ad42a0a408982c775fa910ad452a000
olcay-elt <olcay.elt@gmail.com>
2023-03-09 11:11:07 +0100

    Add

--- class-notes/DE-15-16-RecipeAppProject/src/pages/router/AppRouter.jsx
+++ class-notes/DE-15-16-RecipeAppProject/src/pages/router/AppRouter.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/home/Home";
-import Navbar from "./components/navbar/Navbar";
-import Login from "./pages/login/Login";
-import About from "./pages/about/About";
-import Details from "./pages/details/Details";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "../components/navbar/Navbar";
+import Home from "../pages/home/Home";
+import About from "../pages/about/About";
+import Details from "../pages/details/Details";
+import Login from "../pages/login/Login";
 const AppRouter = () => {
     return (
         <BrowserRouter>
@@ -12,8 +12,10 @@ const AppRouter = () => {
 
             <Routes>
                 <Route path="/" element={<Login />} />
+
                 <Route path="/home" element={<Home />} />
                 <Route path="/about" element={<About />} />
+
                 <Route path="/details" element={<Details />} />
             </Routes>
         </BrowserRouter>
