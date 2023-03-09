d7ce1d1da543bba914989ff0c6b1e008e9591938
olcay-elt <olcay.elt@gmail.com>
2023-03-09 21:29:58 +0100

    Add

--- class-notes/DE-15-16-RecipeAppProject/src/components/navbar/Navbar.jsx
+++ class-notes/DE-15-16-RecipeAppProject/src/components/navbar/Navbar.jsx
@@ -4,26 +4,26 @@ import { GiHamburgerMenu } from "react-icons/gi";
 import { HamburgerDiv, Logo, Menu, MenuLink, Nav } from "./NavbarStyles";
 
 const Navbar = () => {
-  const[acik,setAcik]=useState(false)
-  return (
-    <Nav>
-      <Logo to="/home" onClick={() => setAcik(!acik)}>
-        <i>{"<Clarusway/>"}</i>
-        <span>recipe</span>
-      </Logo>
-      <HamburgerDiv onClick={() => setAcik(!acik)}>
-        <GiHamburgerMenu />
-      </HamburgerDiv>
+    const [acik, setAcik] = useState(false)
+    return (
+        <Nav>
+            <Logo to="/home" onClick={() => setAcik(false)}>
+                <i>{"<Clarusway/>"}</i>
+                <span>recipe</span>
+            </Logo>
+            <HamburgerDiv onClick={() => setAcik(!acik)}>
+                <GiHamburgerMenu />
+            </HamburgerDiv>
 
-      <Menu osman={acik} onClick={() => setAcik(!acik)}>
-        <MenuLink to="/about">About</MenuLink>
+            <Menu osman={acik} onClick={() => setAcik(!acik)}>
+                <MenuLink to="/about">About</MenuLink>
 
-        <a href="https://github.com/">Github</a>
+                <a href="https://github.com/">Github</a>
 
-        <MenuLink to="/">Logout</MenuLink>
-      </Menu>
-    </Nav>
-  );
+                <MenuLink to="/">Logout</MenuLink>
+            </Menu>
+        </Nav>
+    );
 };
 
 export default Navbar;
