5137d87ee062f0b0268010baea81d25f4ff158f7
olcay-elt <olcay.elt@gmail.com>
2023-03-09 21:36:19 +0100

    Add

--- class-notes/DE-15-16-RecipeAppProject/src/pages/login/Login.jsx
+++ class-notes/DE-15-16-RecipeAppProject/src/pages/login/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
     FormContainer,
     Header,
@@ -9,18 +9,44 @@ import {
     StyledInput,
 } from "./LoginStyles";
 import mealSvg from "../../assets/meal.svg";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+    const [name, setName] = useState();
+    const [pass, setPass] = useState();
+    const navigate = useNavigate()
+
+    const backend = (e) => {
+        e.preventDefault()
+        //! fake backend yapıyoruz localstorage a name ve pass kaydediyorum
+        localStorage.setItem("userName", (name))
+        localStorage.setItem("password", (pass))
+
+        navigate("/home")
+
+    }
+
+
     return (
         <LoginContainer>
             <FormContainer>
                 <StyledImg src={mealSvg} />
                 <Header>{"<Clarusway/>"}Recipe</Header>
 
-                <StyledForm>
-                    <StyledInput type="text" placeholder="username" required />
+                <StyledForm onSubmit={backend} >
+                    <StyledInput
+                        type="text"
+                        placeholder="username"
+                        required
+                        onChange={(e) => setName(e.target.value)}
+                    />
 
-                    <StyledInput type="password" placeholder="password" required />
+                    <StyledInput
+                        type="password"
+                        placeholder="password"
+                        required
+                        onChange={(e) => setPass(e.target.value)}
+                    />
 
                     <StyledButton type="submit">Login</StyledButton>
                 </StyledForm>
@@ -30,6 +56,3 @@ const Login = () => {
 };
 
 export default Login;
-
-
-
