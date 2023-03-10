7b87e4788ace778395a1648630950b2df3b3367e
olcay-elt <olcay.elt@gmail.com>
2023-03-10 10:43:33 +0100

    Add new folder

---
+++ class-notes/DE-17-context-reducer/context-example2/src/App.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import ShowUsers from "./pages/ShowUsers";
+
+function App() {
+  return (
+    <>
+      <ShowUsers />
+    </>
+  );
+}
+export default App;
---
+++ class-notes/DE-17-context-reducer/context-example2/src/pages/ShowUsers.js
@@ -0,0 +1,11 @@
+import User from "../components/User";
+
+const ShowUsers = () => {
+  return (
+    <div>
+      <h2>User List</h2>
+    </div>
+  );
+};
+
+export default ShowUsers;
