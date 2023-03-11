1156ec4600e68fd30ad4741f4b1578c7308bebff
olcay-elt <olcay.elt@gmail.com>
2023-03-11 13:24:41 +0100

    Add

--- class-notes/DE-17-context-reducer/reducer-example/src/App.js
+++ class-notes/DE-17-context-reducer/reducer-example/src/App.js
@@ -1,10 +1,12 @@
+import UseReducerExample from "./UseReducerExample";
 import UseStateExample from "./UseStateExample";
 
 function App() {
-  return (
-    <div>
-      <UseStateExample />
-    </div>
-  );
+    return (
+        <div>
+            <UseReducerExample />
+            {/* <UseStateExample /> */}
+        </div>
+    );
 }
 export default App;
