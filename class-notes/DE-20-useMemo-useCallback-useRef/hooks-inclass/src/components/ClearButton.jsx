266f9b1c2f6488f9a2401abc6fd755b576fec583
olcay-elt <olcay.elt@gmail.com>
2023-03-16 15:45:12 +0100

    Add

--- class-notes/DE-20-useMemo-useCallback-useRef/hooks-inclass/src/components/ClearButton.jsx
+++ class-notes/DE-20-useMemo-useCallback-useRef/hooks-inclass/src/components/ClearButton.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { memo } from 'react'
 
 const ClearButton = ({ handleClear }) => {
     console.log("Render => ClearButton componenti")
@@ -9,4 +9,4 @@ const ClearButton = ({ handleClear }) => {
     )
 }
 
-export default ClearButton
+export default memo(ClearButton)
