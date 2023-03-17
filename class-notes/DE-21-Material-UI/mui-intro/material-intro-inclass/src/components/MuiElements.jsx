8a0410eff8da42ed15389efa089bc19b064d4543
olcay-elt <olcay.elt@gmail.com>
2023-03-17 09:37:59 +0100

    Add

--- class-notes/DE-21-Material-UI/mui-intro/material-intro-inclass/src/components/MuiElements.jsx
+++ class-notes/DE-21-Material-UI/mui-intro/material-intro-inclass/src/components/MuiElements.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import Typography from '@mui/material/Typography';
+// or
+// import { Typography } from '@mui/material';//performans açısından problemli
 
 const MuiElements = () => {
-  return (
-    <div>MuiElements</div>
-  )
+    return (
+        <>
+            <Typography
+                variant="h3"
+                component="h1"
+                align="center"
+                color="error"
+                mt={5}>
+                MuiElements
+            </Typography>
+            <Typography
+                variant="body1"
+                align="justify"
+                color="error"
+                m={5}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates cupiditate explicabo quos earum ex consequatur, inventore incidunt, rem pariatur sunt debitis. Fuga praesentium placeat neque.
+            </Typography>
+        </>
+    );
 }
 
 export default MuiElements
