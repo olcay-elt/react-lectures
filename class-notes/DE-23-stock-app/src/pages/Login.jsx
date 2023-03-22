292c73e51949f67823ef91d5b62c047703c0713c
olcay-elt <olcay.elt@gmail.com>
2023-03-22 20:38:57 +0100

    Add

--- class-notes/DE-23-stock-app/src/pages/Login.jsx
+++ class-notes/DE-23-stock-app/src/pages/Login.jsx
@@ -103,10 +103,11 @@ const Login = () => {
             <img src={image} alt="img" />
           </Container>
         </Grid>
+
       </Grid>
     </Container>
   )
 }
 >>>>>>> af676470ef24a739715a46643f78aea5a37ebdec
 
-export default Login
+export default Login 
