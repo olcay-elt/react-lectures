5065bf39185182e16d2b91c57d35af19c146843a
olcay-elt <olcay.elt@gmail.com>
2023-03-22 21:34:14 +0100

    Add

--- class-notes/DE-23-stock-app/src/App.js
+++ class-notes/DE-23-stock-app/src/App.js
@@ -6,26 +6,26 @@ import store from "./app/store";
 import { ToastContainer } from "react-toastify";
 
 function App() {
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: grey["900"],
-      },
-      secondary: {
-        main: blueGrey["900"],
-      },
-    },
-  });
-  return (
-    <>
-      <ThemeProvider theme={theme}>
-        <Provider store={store}>
-          <AppRouter />
-        </Provider>
-        <ToastContainer />
-      </ThemeProvider>
-    </>
-  );
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: grey["900"],
+            },
+            secondary: {
+                main: blueGrey["900"],
+            },
+        },
+    });
+    return (
+        <>
+            <ThemeProvider theme={theme}>
+                <Provider store={store}>
+                    <AppRouter />
+                </Provider>
+                <ToastContainer />
+            </ThemeProvider>
+        </>
+    );
 }
 
 export default App;
