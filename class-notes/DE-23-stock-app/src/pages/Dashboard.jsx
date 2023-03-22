980b2aef8442f314294a218fac45f33c00eabe28
olcay-elt <olcay.elt@gmail.com>
2023-03-22 21:35:12 +0100

    Add

--- class-notes/DE-23-stock-app/src/pages/Dashboard.jsx
+++ class-notes/DE-23-stock-app/src/pages/Dashboard.jsx
@@ -10,21 +10,21 @@ import Typography from "@mui/material/Typography";
 import { useSelector } from "react-redux";
 
 function Dashboard() {
-  const { currentUser } = useSelector((state) => state.auth);
+    const { currentUser } = useSelector((state) => state.auth);
 
-  return (
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <AppBar position="fixed">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            STOCK APP
-          </Typography>
-          {currentUser && <Button color="inherit">Logout</Button>}
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+    return (
+        <Box sx={{ display: "flex" }}>
+            <CssBaseline />
+            <AppBar position="fixed">
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        STOCK APP
+                    </Typography>
+                    {currentUser && <Button color="inherit">Logout</Button>}
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
 }
 
 export default Dashboard;
