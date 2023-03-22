a0337f1c0961fac1ae5aa6a92b9d3f743d29d28b
olcay-elt <olcay.elt@gmail.com>
2023-03-22 21:33:29 +0100

    Add

--- class-notes/DE-23-stock-app/src/router/PrivateRouter.jsx
+++ class-notes/DE-23-stock-app/src/router/PrivateRouter.jsx
@@ -2,9 +2,9 @@ import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
 const PrivateRouter = () => {
-  const { currentUser } = useSelector((state) => state.auth);
+    const { currentUser } = useSelector((state) => state.auth);
 
-  return currentUser ? <Outlet /> : <Navigate to="/" />;
+    return currentUser ? <Outlet /> : <Navigate to="/" />;
 };
 
 export default PrivateRouter;
