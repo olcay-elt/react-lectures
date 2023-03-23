f4ec18c8f2f69a486a5fbfd0e9fab10334bd5255
olcay-elt <olcay.elt@gmail.com>
2023-03-23 22:44:25 +0100

    Add

--- class-notes/DE-23-stock-app/src/app/store.jsx
+++ class-notes/DE-23-stock-app/src/app/store.jsx
@@ -2,9 +2,9 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 
 const store = configureStore({
-  reducer: {
-    auth: authReducer,
-  },
-  devTools: process.env.NODE_ENV !== "production",
+    reducer: {
+        auth: authReducer,
+    },
+    devTools: process.env.NODE_ENV !== "production",
 });
 export default store;
