6f2bc5c94d801850a5a54d749e61b7ddeff09868
olcay-elt <olcay.elt@gmail.com>
2023-04-06 13:50:29 +0200

    Add

--- class-notes/DE-18-19-movie-app/movie-app-in-class/src/App.js
+++ class-notes/DE-18-19-movie-app/movie-app-in-class/src/App.js
@@ -4,16 +4,15 @@ import AppRouter from "./router/AppRouter";
 import { ToastContainer } from "react-toastify";
 import MovieContextProvider from "./context/MovieContext";
 
-const App = () => {
-    return (
-        <div className="dark:bg-gray-dark-main">
-            <AuthContextProvider>
-                <MovieContextProvider>
-                    <AppRouter />
-                    <ToastContainer />
-                </MovieContextProvider>
-            </AuthContextProvider>
-        </div>
+
+<div className="dark:bg-gray-dark-main">
+    <AuthContextProvider>
+        <MovieContextProvider>
+            <AppRouter />
+            <ToastContainer />
+        </MovieContextProvider>
+    </AuthContextProvider>
+</div>
     );
 };
 export default App;
