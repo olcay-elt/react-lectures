c9697f68ce22e309a63941bd7cb74461d3e9ff4d
olcay-elt <olcay.elt@gmail.com>
2023-04-09 11:31:18 +0200

    Add

--- class-notes/DE-18-19-movie-app/movie-app-in-class/src/App.js
+++ class-notes/DE-18-19-movie-app/movie-app-in-class/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import AuthContextProvider from "./context/AuthContext";
 import AppRouter from "./router/AppRouter";
-
+import { ToastContainer } from "react-toastify";
+import MovieContextProvider from "./context/MovieContext";
 
 const App = () => {
     return (
