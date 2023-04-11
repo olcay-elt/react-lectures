85f6e8567df19f99fcb472fa381555e4d3eb6b6a
olcay-elt <olcay.elt@gmail.com>
2023-04-11 23:45:39 +0200

    Add

--- class-notes/DE-14-styledComponent-2/src/components/Card.jsx
+++ class-notes/DE-14-styledComponent-2/src/components/Card.jsx
@@ -5,11 +5,17 @@ import KartSSS from "./styles/KartSSS.jsx";
 const Card = () => {
     return (
         <div>
+            {data.map((a) => (
+                <KartSSS key={a.id} ters={a.id % 2 === 1 && "row-reverse"}>
+                    <div>
+                        <h2>{a.title} </h2>
+                        <p> {a.body} </p>
+                    </div>
 
-            <ImageSSS src={`./images/${a.image}`}></ImageSSS>
-        </KartSSS>
-    ))
-}
+                    <ImageSSS src={`./images/${a.image}`}></ImageSSS>
+                </KartSSS>
+            ))
+            }
         </div >
     );
 };
